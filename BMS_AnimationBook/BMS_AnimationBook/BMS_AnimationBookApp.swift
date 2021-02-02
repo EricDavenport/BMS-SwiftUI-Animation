4cd0c935e8a17e15238fc9942a965bd737427832
Eric Davenport <edavenport6791@gmail.com>
2021-02-02 10:50:49 -0500

    Initial commit

---
+++ BMS_AnimationBook/BMS_AnimationBook/BMS_AnimationBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMS_AnimationBookApp.swift
+//  BMS_AnimationBook
+//
+//  Created by Eric Davenport on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct BMS_AnimationBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
